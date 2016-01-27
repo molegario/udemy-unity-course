e5fe60e84554bdc765375d3462549cdc87f98058
m.olegario <m.olegario@davidstea.com>
2016-01-27 16:20:14 -0500

    refactored code. updated gitignore to ignore temp directory in unity build

--- Number Wizard/Assets/Scripts/NumberWizards.cs
+++ Number Wizard/Assets/Scripts/NumberWizards.cs
@@ -3,22 +3,23 @@ using System.Collections;
 
 public class NumberWizards : MonoBehaviour {
 	
-	int guess = 1000/2;
+	int guess;
 	int min = 1;
 	int max = 1000;
 	bool found = false;
 	// Use this for initialization
 	void Start () {
-		print ("Welcome to Number Wizard");
+		DisplayNewGame();
+	}
+	
+	void DisplayNewGame () {
 		print ("Pick a number in your head, but don't tell me.");
-		
 		print ("The Highest number you can pick is " + max + ".");
 		print ("The lowest number you can pick is " + min + ".");
-		
-		print ("Is the number higher or lower than " + guess + "?");
-		
+		max = max + 1;
+		guess = max/2;
+		print ("Is the number higher or lower than or equal to " + guess + "?");
 		print ("Press UP arrow for higher, DOWN arrow for lower, and RETURN for equals.");
-		
 	}
 	
 	// Update is called once per frame
@@ -29,7 +30,6 @@ public class NumberWizards : MonoBehaviour {
 				print ("I won!!! Your number is " + guess);
 				found = true;
 			} else if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.DownArrow)) {
-				
 				if(Input.GetKeyDown(KeyCode.UpArrow)) {
 					print ("HIGHER");
 					min = guess;
@@ -38,24 +38,14 @@ public class NumberWizards : MonoBehaviour {
 					print ("LOWER");
 					max = guess;				
 				}	
-				guess = (max - min)/2 + min;
-				print ("Is the number higher or lower than or equal to " + (guess));
-				
+				guess = (max + min)/2;
+				print ("Is the number higher or lower than or equal to " + guess);
 			}
 		} else {
 			min = 1;
 			max = 1000;
-			guess = 1000/2;
 			found = false;
-			print ("Pick a number in your head, but don't tell me.");
-			print ("The Highest number you can pick is " + max + ".");
-			print ("The lowest number you can pick is " + min + ".");
-			
-			print ("Is the number higher or lower than " + guess + "?");
-			
-			print ("Press UP arrow for higher, DOWN arrow for lower, and RETURN for equals.");
+			DisplayNewGame();
 		}
-
 	}
-	
 }
