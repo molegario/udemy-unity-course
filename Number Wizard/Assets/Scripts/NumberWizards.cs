1846ea6dafb4ae2523bda09035aa432328658a70
m.olegario <m.olegario@davidstea.com>
2016-01-27 14:14:26 -0500

    completed first assignment - number wizard

---
+++ Number Wizard/Assets/Scripts/NumberWizards.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NumberWizards : MonoBehaviour {
+	
+	int guess = 1000/2;
+	int min = 1;
+	int max = 1000;
+	bool found = false;
+	// Use this for initialization
+	void Start () {
+		print ("Welcome to Number Wizard");
+		print ("Pick a number in your head, but don't tell me.");
+		
+		print ("The Highest number you can pick is " + max + ".");
+		print ("The lowest number you can pick is " + min + ".");
+		
+		print ("Is the number higher or lower than " + guess + "?");
+		
+		print ("Press UP arrow for higher, DOWN arrow for lower, and RETURN for equals.");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!found){
+			if(Input.GetKeyDown(KeyCode.Return)) {
+				print ("RETURN");
+				print ("I won!!! Your number is " + guess);
+				found = true;
+			} else if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.DownArrow)) {
+				
+				if(Input.GetKeyDown(KeyCode.UpArrow)) {
+					print ("HIGHER");
+					min = guess;
+					
+				} else if (Input.GetKeyDown(KeyCode.DownArrow)) {
+					print ("LOWER");
+					max = guess;				
+				}	
+				guess = (max - min)/2 + min;
+				print ("Is the number higher or lower than or equal to " + (guess));
+				
+			}
+		} else {
+			min = 1;
+			max = 1000;
+			guess = 1000/2;
+			found = false;
+			print ("Pick a number in your head, but don't tell me.");
+			print ("The Highest number you can pick is " + max + ".");
+			print ("The lowest number you can pick is " + min + ".");
+			
+			print ("Is the number higher or lower than " + guess + "?");
+			
+			print ("Press UP arrow for higher, DOWN arrow for lower, and RETURN for equals.");
+		}
+
+	}
+	
+}
